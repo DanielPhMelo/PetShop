fa98ed1562adde1101615d025bc1661558d981b6
Daniel Melo <dphilomena@gmail.com>
2019-11-22 22:30:49 -0300

    Class 15 - end version

---
+++ src/main/java/br/com/tt/petshop/controller/UnidadeController.java
@@ -0,0 +1,47 @@
+package br.com.tt.petshop.controller;
+
+import br.com.tt.petshop.model.Unidade;
+import br.com.tt.petshop.service.UnidadeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping
+public class UnidadeController {
+    private final UnidadeService unidadeService;
+
+    public UnidadeController(UnidadeService unidadeService) {
+        this.unidadeService = unidadeService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET,
+            value = "/admin/unidades")
+    public String listar(Model model){
+        model.addAttribute("mensagem",
+                "Bem vindo a lista de unidades da petshop");
+        model.addAttribute("unidades", unidadeService.listar());
+        return "unidade";
+    }
+
+    @RequestMapping(method = RequestMethod.GET,
+            value = "/admin/unidades/criar")
+    public String unidadeCriar(Model model){
+        model.addAttribute("novaUnidade", new Unidade());
+        return "unidade_criar";
+    }
+
+    @RequestMapping(method = RequestMethod.POST,
+            value = "/admin/unidades/criarNovo")
+    public String criarNovo(Unidade unidade, Model model) {
+            unidadeService.salvar(unidade);
+            model.addAttribute("mensagem",
+                    "Unidade salva com sucesso!");
+
+        model.addAttribute("unidades",
+                unidadeService.listar());
+
+        return "unidade";
+    }
+}
--- src/main/java/br/com/tt/petshop/repository/UnidadeRepository.java
+++ src/main/java/br/com/tt/petshop/repository/UnidadeRepository.java
@@ -4,4 +4,5 @@ import br.com.tt.petshop.model.Unidade;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UnidadeRepository extends JpaRepository<Unidade, Long> {
+
 }
